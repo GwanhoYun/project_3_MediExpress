8af8b66a5129cae105d585fa714adb709ceaca85
GR <GR@GRN-304-05>
2024-07-24 09:50:18 +0900

    hello

---
+++ src/main/java/org/hj/controller/HomeController.java
@@ -0,0 +1,25 @@
+package org.hj.controller;
+
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	
+	@GetMapping("/") 
+
+	public String home(Locale locale, Model model) {
+		return "Testfile"; 
+	}
+	
+
+	
+	
+}
